84158ceab92518cd1a83a19a4865279692f279da
alicelee1231 <saemmilee1231@gmail.com>
2023-09-25 22:07:00 +0900

    read updated

--- read.py
+++ read.py
@@ -1,27 +1,23 @@
 import os
 
-def read_text_file1(file_path):
+def read_text_file(filepath):
     outputs = []
-
-    for file in os.listdir(file_path):
+    for file in os.listdir(filepath):
         if file.endswith(".txt"):
-            target_path = f"{file_path}\{file}"
-
-           # print(target_path)
-        with open(target_path,'r')as f:
-            outputs.append(f.read().strip('\n'))
+            target_path = f"{filepath}/{file}"
+            with open(target_path, 'r') as f:
+                outputs.append(f.read().strip('\n'))
     return outputs
-print(read_text_file1("../source/27-1"))
+
+print(read_text_file("../source/27-1"))
 print()
 
 import glob
 
-def read_text_file2(file_path):
+def read_text_file2(filepath):
     outputs = []
-    for file in glob.glob(file_path + '\*.txt'):
+    for file in glob.glob(filepath + '/*.txt'):
         with open(file,'r')as f :
             outputs.append(f.read().strip('\n'))
     return outputs
-
-print(read_text_file2('../source/27-1'))
-print()
+print(read_text_file2("../source/27-1"))
