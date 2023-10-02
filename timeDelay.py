61f765366f2ab64031cd5250909f995c6a75cb2b
alicelee1231 <saemmilee1231@gmail.com>
2023-10-02 13:16:13 +0900

    timeDelay study

---
+++ timeDelay.py
@@ -0,0 +1,36 @@
+
+import time
+
+a=0
+
+while a < 10:
+    a = a+1
+    time.sleep(1)
+    print(a)
+
+# b=0
+# while True:
+#     n += 1
+#     time.sleep(1)
+#     print(n)
+#     if n ==10:
+#         break
+
+# n =1
+# while True:
+#     time.sleep(1)
+#     print(n)
+
+#     n+=1
+#     if n >10:
+#         break
+
+
+# n =1
+# while True:
+#     time.sleep(1)
+#     if n <=10:
+#         print(n)
+#         n +=1
+#         continue
+#     break
